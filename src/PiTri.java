87bfaa7af83083542420968e09223fbebcf222f7
Roman Golubev <roman222golubev@mail.ru>
2025-03-02 15:40:03 +0300

    проверка профиля

--- src/PiTri.java
+++ src/PiTri.java
@@ -1,20 +1,22 @@
 public class PiTri {
 
     public static void main(String[] args) {
-        if(args == null || args.length == 0){
+        if (args == null || args.length == 0) {
             System.out.println("Введите количество пифагоровых троек для вывода или введите три числа для проверки.");
             return;
         }
-        if(args.length == 1){ //вывести N троек
+
+
+        if (args.length == 1) { //вывести N троек
             int N = Integer.parseInt(args[0]);
             int a = 1;
             int i = 0;
-            while (i < N && a < 1000){
+            while (i < N && a < 1000) {
                 int b = 1;
                 while (i < N && b < 1000) {
                     int c = 1;
-                    while (i < N && c < 1000){
-                        if(checkTriple(a, b, c)){
+                    while (i < N && c < 1000) {
+                        if (checkTriple(a, b, c)) {
                             System.out.println("Числа " + a + ", " + b + ", " + c + " являются пифагоровой тройкой");
                             i++;
                         }
@@ -28,8 +30,8 @@ public class PiTri {
             int a = Integer.parseInt(args[0]);
             int b = Integer.parseInt(args[1]);
             int c = Integer.parseInt(args[2]);
-            if(checkTriple(a, b, c)){
-                System.out.println(a + ", "+ b + ", "+ c + " — это настоящая пифагорова тройка!");
+            if (checkTriple(a, b, c)) {
+                System.out.println(a + ", " + b + ", " + c + " — это настоящая пифагорова тройка!");
             } else {
                 System.out.println("Эти числа не являются пифагоровой тройкой");
             }
